06375bb40eb0d9c451b204eca4ae0ebe5490e6fa
Felix Andersson <felix.andersson@gmail.com>
2025-10-23 11:42:31 +0200

    trying to create a conflict with rebase

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ package example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Felix!");
+        System.out.println("Hello, World!");
     }
 }
