862afde827a6ab457b026cd8181f2a79e0c5665f
Felix Andersson <felix.andersson@gmail.com>
2025-10-23 11:52:00 +0200

    kept both phrases

--- App.java
+++ App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello, Universe!");
+=======
+        System.out.println("Hello, Git!");
+>>>>>>> c085c92 (Git)
     }
 }
